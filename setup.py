25de7f569ac261d8e1d1db9b3a79a88949927d6b
yuralukashik <yuralukashik@gmail.com>
2015-10-03 02:30:35 +0300

    updated version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='copusher',
-    packages=['copusher'],  # this must be the same as the name above
-    version='0.1.1',
+    packages=['copusher'],
+    version='0.1.2',
     description='A robust Command Output PUSHER',
     author='Yura Lukashik',
     author_email='YuraLukashik@gmail.com',
-    url='https://github.com/YuraLukashik/copusher',  # use the URL to the github repo
-    download_url='https://github.com/YuraLukashik/copusher/tarball/0.1.1',  # I'll explain this in a second
-    keywords=['command', 'output', 'push'],  # arbitrary keywords
+    url='https://github.com/YuraLukashik/copusher',
+    download_url='https://github.com/YuraLukashik/copusher/tarball/0.1.2',
+    keywords=['command', 'output', 'push'],
     classifiers=[],
     scripts=['bin/copusher']
 )
