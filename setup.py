e8dcda17aac0e59141f9adccd80e59dfb4590c96
yuralukashik <yuralukashik@gmail.com>
2015-10-03 01:27:12 +0300

    files for creating python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='copusher',
+    packages=['copusher'],  # this must be the same as the name above
+    version='0.1',
+    description='A robust Command Output PUSHER',
+    author='Yura Lukashik',
+    author_email='YuraLukashik@gmail.com',
+    url='https://github.com/YuraLukashik/copusher',  # use the URL to the github repo
+    download_url='https://github.com/YuraLukashik/copusher/tarball/0.1',  # I'll explain this in a second
+    keywords=['command', 'output', 'push'],  # arbitrary keywords
+    classifiers=[],
+)
