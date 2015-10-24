5b43300d01cd178a88389120d75f8ffdcde84456
Yura Lukashik <YuraLukashik@gmail.com>
2015-10-24 14:54:57 +0300

    added command line -f argument

--- copusher/copusher.py
+++ copusher/copusher.py
@@ -1,6 +1,7 @@
 import sys
 import pyperclip
 import importlib
+import optparse
 
 class Copusher:
     def __init__(self):
@@ -9,15 +10,20 @@ class Copusher:
         }
 
     def run(self):
-        storage_module = importlib.import_module("copusher.storages." + self.config['storage'])
+        storage_module = importlib.import_module("storages." + self.config['storage'])
         storage = storage_module.Storage()
+
+        p = optparse.OptionParser()
+        p.add_option("-f", action="store", dest="filename")
+        p.set_defaults(filename = "my_terminal_output.txt")
+        opts, args = p.parse_args()
 
         content = ""
 
         for line in sys.stdin:
             content += line
 
-        uri = storage.store(content)
+        uri = storage.store(opts.filename, content)
 
         pyperclip.copy(uri)
         print(uri)
